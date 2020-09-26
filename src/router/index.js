da1e78f61394d9f2f8030be761a22296434b6d38
wangshiyuan <wangshiyuan@navinfo.com>
2020-09-26 23:23:29 +0800

    修改datepicker样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import HelloWorld from '../components/HelloWorld'
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/home',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/helloworld',
+    name: 'HelloWorld',
+    component: HelloWorld,
   },
   {
     path: '/about',
